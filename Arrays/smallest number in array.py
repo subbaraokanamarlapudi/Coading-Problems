f69dcffcc89f9cb7d4b4ee3c57db0efc25ebd2d5
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-04-22 21:09:23 +0530

    added smallest number in array

---
+++ Arrays/smallest number in array.py
@@ -0,0 +1,8 @@
+def small_number(arr):
+    return min(arr)
+
+arr1 = small_number([2,5,1,3,0])
+print("The smallest element is: ", arr1)
+
+arr2 = small_number([8,10,5,7,9])
+print("The smallest element is: ", arr2)
